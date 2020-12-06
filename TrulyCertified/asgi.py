fa6115b37f2aef35f510358dcb553942e6132ae3
Comrane <vladimir.dolgov@yahoo.com>
2020-12-06 20:52:07 +0200

    Initial commit

---
+++ TrulyCertified/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TrulyCertified project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TrulyCertified.settings')
+
+application = get_asgi_application()
